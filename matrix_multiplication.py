767a123a34ed6e0448d7baa88f02973a1dc42b73
JaWrBr <77751635+JaWrBr@users.noreply.github.com>
2021-08-02 12:48:27 +0200

    Update matrix_multiplication.py

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,45 +1,82 @@
-'''
-Write a small interactive python utility that calculates matrix multiplication.
-None of the variables are passed as arguments, everything will be handled at runtime
+# result Matrix
+def main():
+  result = []
+  a_width, a_height = (input ('\nMatrix A\nwidth: ')), (input ('height: '))
+  if not a_width.isnumeric() or not a_height.isnumeric():
+    print ('Input must be a number (positive integer).') 
+    exit()
+  else:
+    a_width, a_height = int(a_width), int(a_height)
 
-Write the utility in a way that you’d write a productiuon code, use python3 without any mathematical
-libraries (like numpy), if you are familiar with pytest, use test driven development approach and attach all tests created during development.
-'''
+  b_width, b_height = input ('\nMatrix B\nwidth: '), input ('height: ')
+  if not b_width.isnumeric() or not b_height.isnumeric():
+    print ('Input must be a number (positive integer).') 
+    exit()
+  else:
+    b_width, b_height = int(b_width), int(b_height)
+  
+  r_width, r_height = b_width, a_height
+  a_values = matrixA(a_height,a_width)
+  b_values = matrixB(b_height,b_width)
+  
+  for i in range (r_height):
+    result.append([])
+    for j in range (r_width):
+      result[i].append(0)
 
-a_width, a_height = int(input ('\nMatrix A\nwidth: ')), int(input ('height: '))
-b_width, b_height = int(input ('\nMatrix B\nwidth: ')), int(input ('height: '))
-r_width, r_height = b_width, a_height
-a_values, b_values = [], []
-result = []
+  for i in range(len(a_values)):
+        for j in range(len(b_values[0])):
+          for k in range(len(b_values)):
+              result[i][j] += a_values[i][k] * b_values[k][j]
+  
+  print ('\nResult: ')    
+  for line in result:
+    l = ''
+    for item in line:
+      l = l + str(item) + ' '
+    print(l)
 
 # Matrix A
-print ('\nMatrix A values:')
-for i in range ((a_height)):
-  a_values.append(input().split())
+def matrixA(a_height,a_width):
+  a_values = []
+  print ('\nMatrix A values:')
+  for i in range (a_height):
+    a_values.append(input().split())
+    if len(a_values[i])>a_width:
+      print (f'Input must not exceed given Matrix A width i.e. {a_width} numbers!')
+      exit()
 
-for i in range (a_height):
-  for j in range (a_width):
-    a_values[i][j] = int(a_values[i][j])
+    
+
+  for i in range (a_height):
+    for j in range (a_width):
+      if not a_values[i][j].isnumeric():
+        print ('Input must be a number(s)-positive integer(s), separated by space.')
+        exit()
+      else:
+        a_values[i][j] = int(a_values[i][j])
+  return a_values
 
 # Matrix B
-print ('\nMatrix B values:')
-for i in range (b_height):
-  b_values.append(input().split())
+def matrixB(b_height,b_width):
+  b_values = []
+  print ('\nMatrix B values:')
+  for i in range (b_height):
+    b_values.append(input().split())
+    if len(b_values[i])>b_width:
+      print (f'Input must not exceed given Matrix B width i.e. {b_width} number(s)!')
+      exit()
 
-for i in range (b_height):
-  for j in range (b_width):
-    b_values[i][j] = int(b_values[i][j])
 
-# result Matrix
-for i in range (r_height):
-  result.append([])
-  for j in range (r_width):
-    result[i].append(0)
+  for i in range (b_height):
+    for j in range (b_width):
+      if not b_values[i][j].isnumeric():
+        print ('Input must be a number(s)-positive integer(s), separated by space.')
+        exit()
+      else:
+        b_values[i][j] = int(b_values[i][j])
+  return b_values
 
-for i in range(len(a_values)):
-      for j in range(len(b_values[0])):
-        for k in range(len(b_values)):
-            result[i][j] += a_values[i][k] * b_values[k][j]
-  
-for r in result:
-    print(r)
+
+main()
+
